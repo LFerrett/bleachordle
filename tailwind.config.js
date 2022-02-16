7d402dfee62c4c48af5c4cbee9980b968a05c5cc
Lauren Ferrett <93666073+LFerrett@users.noreply.github.com>
2022-02-15 22:47:49 -0500

    Style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          light: '#ddbf5f',
+          base: '#d4af37',
+          dark: '#aa8c2c'
+      },
+      },
+    },
   },
   plugins: [],
 }
